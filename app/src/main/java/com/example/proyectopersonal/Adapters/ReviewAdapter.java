014d2cb8bcbcf55cce7e0b1ebe87517bde5adfce
Rodrigo5023 <a20163458@pucp.edu.pe>
2020-07-27 14:11:52 -0500

    Tercer Avance2

---
+++ app/src/main/java/com/example/proyectopersonal/Adapters/ReviewAdapter.java
@@ -0,0 +1,53 @@
+package com.example.proyectopersonal.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.proyectopersonal.Entidades.Cast;
+import com.example.proyectopersonal.Entidades.Review;
+import com.example.proyectopersonal.R;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> {
+
+    Review[] listaReview;
+    Context contexto;
+
+    public ReviewAdapter (Review[] listaReview, Context contexto) {
+        this.listaReview = listaReview;
+        this.contexto = contexto; }
+
+    public static class ReviewViewHolder extends RecyclerView.ViewHolder {
+        public TextView autorReview;
+        public TextView cuerpoReview;
+
+        public ReviewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.autorReview =itemView.findViewById(R.id.textViewNombre);
+            this.cuerpoReview = itemView.findViewById(R.id.textViewPersonaje);}
+    }
+
+    @NonNull
+    @Override
+    public ReviewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(contexto).inflate(R.layout.adapter_cast_rv,parent,false);
+        ReviewAdapter.ReviewViewHolder reviewViewHolder = new ReviewAdapter.ReviewViewHolder(itemView);
+        return reviewViewHolder; }
+
+    @Override
+    public void onBindViewHolder(ReviewViewHolder holder, int position) {
+        final Review review = listaReview[position];
+        String autorReview = review.getAuthor(); holder.autorReview.setText(autorReview);
+        String cuerpoReview = review.getContent(); holder.cuerpoReview.setText(cuerpoReview);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaReview.length;
+    }
+}
