5cd1882066da77906bf002876b891ea4d759dfca
Rodrigo5023 <a20163458@pucp.edu.pe>
2020-07-31 12:48:01 -0500

    Avance jsonObject

---
+++ app/src/main/java/com/example/proyectopersonal/Entidades/MovieDTO.java
@@ -0,0 +1,15 @@
+package com.example.proyectopersonal.Entidades;
+
+public class MovieDTO {
+
+    private Movie[] listaMovies;
+
+
+    public Movie[] getListaMovies() {
+        return listaMovies;
+    }
+
+    public void setListaMovies(Movie[] listaMovies) {
+        this.listaMovies = listaMovies;
+    }
+}
--- app/src/main/java/com/example/proyectopersonal/MainActivity.java
+++ app/src/main/java/com/example/proyectopersonal/MainActivity.java
@@ -20,13 +20,19 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.proyectopersonal.Adapters.MovieAdapter;
 import com.example.proyectopersonal.Entidades.Movie;
+import com.example.proyectopersonal.Entidades.MovieDTO;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,6 +40,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     MovieDB movieDB = new MovieDB();
+    Movie[] listaMovies;
+    int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +51,41 @@ public class MainActivity extends AppCompatActivity {
         // PELICULAS MAS POPULARES
         if(isInternetAvailable()) {
             //https://api.themoviedb.org/3/movie/popular?api_key=06a1953c26075c04668b820d78955ec7&language=en-US&page=1
-            String urlPelicula = movieDB.getUrlMovieDB() + "movie/popular?api_key=" + movieDB.getApiKey() + "&language=en-US&page=1";
-            final RequestQueue queueMoviesPopulares = Volley.newRequestQueue(MainActivity.this);
+            String urlPelicula = movieDB.getUrlMovieDB() + "movie/popular?api_key=" + movieDB.getApiKey() + "&language=es-ES";
 
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, urlPelicula,
-                    new Response.Listener<String>() {
+
+
+            StringRequest stringRequest = new StringRequest(StringRequest.Method.GET, urlPelicula, new Response.Listener<String>() {
+
                         @Override
                         public void onResponse(String response) {
-                            Gson gson = new Gson();
-                            Movie[] arrayMovies = gson.fromJson(response,Movie[].class);
-                            List<Movie> listMoviesPopulares = Arrays.asList(arrayMovies);
 
-                            final MovieAdapter movieAdapter = new MovieAdapter(arrayMovies,MainActivity.this);
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                JSONArray results = (JSONArray) jsonObject.get("results");
+                                int tamañoLista = results.length();
+                                listaMovies = new Movie[tamañoLista];
+
+                                for ( int x=0; x<tamañoLista; x++){
+                                    Movie movie = new Movie();
+                                    JSONObject pelicula = (JSONObject) results.get(x);
+                                    String idMovie = pelicula.getString("id"); movie.setId(Integer.valueOf(idMovie));
+                                    String tituloMovie = pelicula.getString("original_title"); movie.setOriginal_title(tituloMovie);
+                                    String descripcionMovie = pelicula.getString("overview");movie.setOverview(descripcionMovie);
+                                    String posterMovie = pelicula.getString("poster_path"); movie.setPoster_path(posterMovie);
+                                    String lenguajeMovie = pelicula.getString("original_language"); movie.setOriginal_language(lenguajeMovie);
+                                    // String duracionMovie = pelicula.getString("runtime"); movie.setRuntime(Integer.valueOf(duracionMovie));
+                                    String estrenoMovie = pelicula.getString("release_date"); movie.setRelease_date(estrenoMovie);
+                                    String puntuacionMovie = pelicula.getString("vote_average"); movie.setVote_average(puntuacionMovie);
+                                    String votosMovie = pelicula.getString("vote_count"); movie.setVote_count(votosMovie);
+                                    //String fraseMovie = pelicula.getString("tagline"); movie.setTagline(fraseMovie);
+                                    listaMovies[x] = movie;
+                                }
+                            }
+
+                            catch (JSONException e) { e.printStackTrace(); }
+
+                            final MovieAdapter movieAdapter = new MovieAdapter(listaMovies,MainActivity.this);
                             RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewMovies);
                             recyclerView.setAdapter(movieAdapter);
                             recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
@@ -65,7 +96,12 @@ public class MainActivity extends AppCompatActivity {
                         public void onErrorResponse(VolleyError error) {
                             Toast.makeText(MainActivity.this, "Error: Populares", Toast.LENGTH_SHORT).show();
                         }
-                    }); queueMoviesPopulares.add(stringRequest); }
+                    });
+            RequestQueue queueMoviesPopulares = Volley.newRequestQueue(MainActivity.this);
+            queueMoviesPopulares.add(stringRequest);
+
+
+             }
     }
 
 
